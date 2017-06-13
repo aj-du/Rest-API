54525d32dc0987889153d35f48466be8e657478a
Artur Borkowski <arthur.borkovsky@gmail.com>
2017-06-13 22:32:11 +0200

    Added CrossOrigin annotation for some localhost domains in PackageRestController. Disabled rebuilding DB with each re-launch.

--- ajdu_restful_api/src/main/java/ajdu_restful_api/controller/PackageRestController.java
+++ ajdu_restful_api/src/main/java/ajdu_restful_api/controller/PackageRestController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,7 @@ import ajdu_restful_api.service.ServiceService;
 import ajdu_restful_api.service.UserService;
 
 @RestController
+@CrossOrigin(origins={"http://localhost:9000", "http://localhost:4200", "http://localhost:8080"}, maxAge=4800)
 public class PackageRestController {
 
 	@Autowired
